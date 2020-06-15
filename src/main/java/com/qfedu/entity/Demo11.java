33010fd5dc17d413dcc74efac4cc233c0312b796
747664025 <747664025@qq.com>
2020-06-15 11:27:01 +0800

    注释一下

---
+++ src/main/java/com/qfedu/entity/Demo11.java
@@ -0,0 +1,27 @@
+package com.qfedu.entity;
+
+public class Demo11 {
+    int a;
+    String b;
+
+    public Demo11(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
